8d3e447de387e753d015428e319e123f5b6bafac
gunhawk2314 <aggarnav@gmail.com>
2017-02-08 21:04:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content= {
+    title: 'Article One | Arnav Aggarwal',
+    date: 'september 5 2016',
+    heading: "article one",
+    content: ` <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p> `
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
