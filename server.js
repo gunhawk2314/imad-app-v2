e614682134767c218e7411c43f96849b9f18f800
gunhawk2314 <aggarnav@gmail.com>
2017-02-09 19:30:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ var content= {
 };
 
 function createTemplate(data){
-    var heading=data.heading
-    var date=data.date
-    var title=data.title
+    var heading=data.heading;
+    var date=data.date;
+    var title=data.title;
     var htmlTemplate = `<html>
     <head>
         <title>
@@ -50,7 +50,7 @@ function createTemplate(data){
                 </div>
             </div>
         </body>
-</html>`
+</html>`;
     return htmlTemplate;
 }
 
@@ -58,7 +58,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(content));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
