cb762316a3ef8ed94b4c7608f1bdb46daa992568
gunhawk2314 <aggarnav@gmail.com>
2017-02-10 12:18:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,37 +30,41 @@ var articles= {
             }
     };
 
-function createTemplate(data){
-    var heading=data.heading;
-    var date=data.date;
-    var title=data.title;
-    var htmlTemplate = `<html>
-    <head>
-        <title>
-           ${title}
-        </title>
-        <meta name="viewport" content="width-device-width, intial-scale-1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-
-    </head>
-        <body>
-            <div class="container">
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <hr>
-                <h3>
-                   ${heading} 
-                </h3>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                    ${content}
-                </div>
-            </div>
-        </body>
-</html>`;
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                ${content}
+              </div>
+          </div>
+          <script type="text/javascript" src="/ui/article.js"></script>
+      </body>
+    </html>
+    `;
     return htmlTemplate;
 }
 
@@ -70,6 +74,10 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
   var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
+});
+app.get('/:articleName', function(req, res) {
+   var articleName= req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
