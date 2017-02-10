ba682e12bfbf0791c83ad31fd6c2d5b6b9c6d8e2
gunhawk2314 <aggarnav@gmail.com>
2017-02-10 14:13:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
+var img=document.getElementById('madi');
+img.onclick= fucntion () {
+    img.style.marginleft='100px';
+}
