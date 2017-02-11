f42f6e6d3884d09b8e024406150ce5b412f5e055
gunhawk2314 <aggarnav@gmail.com>
2017-02-11 14:19:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,7 @@
-console.log('Loaded!');
-var element= document.getElementById('main-text');
-element.innerHTML='NEW VALUE';
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft= marginLeft + 1;
-    img.style.marginLeft= marginLeft + 'px';
-}
-img.onclick= function(){
-    var interval= setInterval(moveRight, 50);
+//counter code
+var button= document.getElementById('counter');
+button.onclick= function() {
+    counter= counter + 1;
+    var span= document.getElementById('span');
+    span.InnerHTML= counter.toString();
 };
