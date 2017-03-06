e64399c3d6119858e8a414f82fad484c5938dbb6
gunhawk2314 <aggarnav@gmail.com>
2017-03-06 22:43:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,8 +96,12 @@ app.post('/login', function(req, res){
            if (result.rows.length===0){
             res.status(403).send('username/password is invalid');   
            } else {
-               var dbString=result.rows[1].password;
-               
+               var dbString=result.rows[0].password;
+               var salt =dbString.split('$')[2];
+               var hashedPassword= hash(password, salt);
+               if (hashedPassword=== dbString){
+                   res.send('credentials verified!');
+               }
            }
        }
    });
