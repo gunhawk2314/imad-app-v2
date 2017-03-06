56c97baa28eae17806fe432bcd436616f500ad09
gunhawk2314 <aggarnav@gmail.com>
2017-03-06 23:07:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,3 @@
-//counter code
-var button = document.getElementById('counter');
-button.onclick= function () {
-    
-    var request=new XMLHttpRequest();
-    
-    request.onreadystatechange = function () {
-        
-        if (request.readyState === XMLHttpRequest.DONE){
-            
-            if(request.status===200){
-                var counter=request.responseText;
-                var span= document.getElementById('count');
-                span.innerHTML= counter.toString();
-                
-            }
-            
-        }
-    };
-    request.open('GET', 'http://gunhawk2314.imad.hasura-app.io/counter', true );
-    request.send(null);
-};
-
 
 var submit= document.getElementById('submit_btn');
 submit.onclick= function () {
@@ -48,8 +25,8 @@ submit.onclick= function () {
     };
     var nameInput= document.getElementById('name');
     var name= nameInput.value;
-    request.open('GET', 'http://gunhawk2314.imad.hasura-app.io/submit-name?name=' + name, true );
-    request.send(null);
+    request.open('POST', 'http://gunhawk2314.imad.hasura-app.io/submit-name?name=' + name, true );
+    request.send(JSON.stingify({username: username, password: password}));
     
 
     
