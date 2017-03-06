9ab570c52cd448566ef7bc5a36007ee704fe243c
gunhawk2314 <aggarnav@gmail.com>
2017-03-06 23:16:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,7 @@ submit.onclick= function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://gunhawk2314.imad.hasura-app.io/login' + name, true );
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
     
 
