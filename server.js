4376b6b777eb5eea7fc02af7c44886c015b46f69
gunhawk2314 <aggarnav@gmail.com>
2017-03-06 12:51:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,23 +3,13 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var config = {
-  host: 'db.imad.hasura-app.io',
   user: 'gunhawk2314',
-  password: process.env.DB_PASSWORD,
   database: 'gunhawk2314',
-  port: '5432'
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
 };
 
-var pool= new Pool(config);
-app.get('/test-db', function(req, res) {
-  pool.query('SELECT * FROM test', function(err,result){
-      if(err){
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result));
-      }
-  });
-});
 
 var app = express();
 app.use(morgan('combined'));
@@ -97,6 +87,18 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool= new Pool(config);
+app.get('/test-db', function(req, res) {
+  pool.query('SELECT * FROM test', function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+  });
+});
+
 
 var counter=0;
 app.get('/counter', function(req, res){
